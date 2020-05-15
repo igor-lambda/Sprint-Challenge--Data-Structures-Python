3a89ee92c48dcff2847c2017d22fa8c4bb3d5944
igor <embiggenerd@gmail.com>
2020-05-15 11:44:05 -0400

    first_attempt

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,29 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.size = 0
+        self.storage = []
+        self.storage_at_capacity = []
+    
+    def increment_size(self):
+        self.size = self.size + 1
+
+    def decrement_size(self):
+        self.size = self.size - 1
+
+    def at_capacity(self):
+        return self.size > self.capacity
 
     def append(self, item):
-        pass
+        self.increment_size()
+        if self.at_capacity():
+            self.storage.remove(self.storage[0])
+            self.decrement_size()
+            self.storage.insert(0, item)
+            return
+        self.storage.append(item)
+
 
     def get(self):
-        pass
+        return self.storage
+            
